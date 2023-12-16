d3a6432d0f1a8ca1ee95657bce6bacccbb33bfbf
Raul Araujo <devraespim@gmail.com>
2023-12-16 17:07:51 -0300

    create fake data to test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Header from './Header';
 import Footer from './Footer';
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (
-    <>
-        <Header />
-        <Note />
-        <Footer />    
-    </>
+        <>
+            <Header />
+            {notes.map(note => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
+            <Footer />
+        </>
     );
 }
 
